c40c68d252db92076dbca976841b789de209ad95
luobosi <luobosi@2dfire.com>
2017-12-04 16:29:59 +0800

    懒加载和 IODH

--- design-patterns/src/main/java/com/luobosi/study/dp/singleton/InitializationOnDemandHolderSingleton.java
+++ design-patterns/src/main/java/com/luobosi/study/dp/singleton/InitializationOnDemandHolderSingleton.java
@@ -3,6 +3,8 @@
  */
 package com.luobosi.study.dp.singleton;
 
+import com.luobosi.common.constant.Constants;
+
 /**
  * InitializationOnDemandHolderSingleton
  * IoDH 模式的单例（静态内部类）
@@ -11,5 +13,26 @@ package com.luobosi.study.dp.singleton;
  * @since 2017-12-04
  */
 public class InitializationOnDemandHolderSingleton {
+
+    private InitializationOnDemandHolderSingleton() {
+
+    }
+
+    /**
+     * 静态内部类，用来初始化 instance 对象，由于静态内部类只会加载一次，而且是在类加载的时候加载，
+     * instance 只会被加载一次
+     */
+    public static class HolderClass {
+        public final static InitializationOnDemandHolderSingleton instants = new InitializationOnDemandHolderSingleton();
+    }
+
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < Constants.ConstantNumber.TEN) {
+            new Thread(()-> System.out.println(HolderClass.instants)).start();
+            i++;
+        }
+    }
+
 
 }
--- design-patterns/src/main/java/com/luobosi/study/dp/singleton/LazySingleton.java
+++ design-patterns/src/main/java/com/luobosi/study/dp/singleton/LazySingleton.java
@@ -3,6 +3,8 @@
  */
 package com.luobosi.study.dp.singleton;
 
+import com.luobosi.common.constant.Constants;
+
 /**
  * LazySingleton
  * 懒汉式单例
@@ -11,5 +13,31 @@ package com.luobosi.study.dp.singleton;
  * @since 2017-12-04
  */
 public class LazySingleton {
+    /** volatile 可以保持可见性 */
+    private static volatile LazySingleton lazySingleton = null;
 
+    private LazySingleton() {}
+
+    public static LazySingleton getInstance() {
+        // 第一重判断
+        if (lazySingleton == null) {
+            // 锁定代码块
+            synchronized (LazySingleton.class) {
+                // 第二重判断
+                if (lazySingleton == null) {
+                    // 创建单例实例
+                    lazySingleton = new LazySingleton();
+                }
+            }
+        }
+        return lazySingleton;
+    }
+
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < Constants.ConstantNumber.TEN) {
+            new Thread(()-> System.out.println(LazySingleton.getInstance())).start();
+            i++;
+        }
+    }
 }
