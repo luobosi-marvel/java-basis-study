f2438d1cb4fd7fc3fe2f168a0c6afc14637d44d4
luobosi <luobosi@2dfire.com>
2017-12-04 13:58:26 +0800

    IoDH 模式的单例（静态内部类）

---
+++ design-patterns/src/main/java/com/luobosi/study/dp/singleton/InitializationOnDemandHolderSingleton.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2009-2016 Hangzhou 2Dfire Technology Co., Ltd. All rights reserved
+ */
+package com.luobosi.study.dp.singleton;
+
+/**
+ * InitializationOnDemandHolderSingleton
+ * IoDH 模式的单例（静态内部类）
+ *
+ * @author luobosi@2dfire.com
+ * @since 2017-12-04
+ */
+public class InitializationOnDemandHolderSingleton {
+
+}
