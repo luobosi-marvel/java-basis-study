12d0f81e696619f41317dffdb0df9dc95e66f614
luobosi <luobosi@2dfire.com>
2017-12-04 13:56:34 +0800

    添加饿汉式单例和懒汉式单例

---
+++ design-patterns/src/main/java/com/luobosi/study/dp/singleton/EagerSingletion.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2009-2016 Hangzhou 2Dfire Technology Co., Ltd. All rights reserved
+ */
+package com.luobosi.study.dp.singleton;
+
+/**
+ * EagerSingletion
+ * 饿汉式单例
+ *
+ * @author luobosi@2dfire.com
+ * @since 2017-12-04
+ */
+public class EagerSingletion {
+
+}
---
+++ design-patterns/src/main/java/com/luobosi/study/dp/singleton/LazySingleton.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2009-2016 Hangzhou 2Dfire Technology Co., Ltd. All rights reserved
+ */
+package com.luobosi.study.dp.singleton;
+
+/**
+ * LazySingleton
+ * 懒汉式单例
+ *
+ * @author luobosi@2dfire.com
+ * @since 2017-12-04
+ */
+public class LazySingleton {
+
+}
