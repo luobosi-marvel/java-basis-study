0bc09f3f708d755e470bb2f8f1f8255236a18e25
luobosi <luobosi@2dfire.com>
2017-12-05 13:14:07 +0800

    反射练习

---
+++ reflection-example/src/main/java/com/luobosi/reflect/model/Person.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2009-2016 Hangzhou 2Dfire Technology Co., Ltd. All rights reserved
+ */
+package com.luobosi.reflect.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * Person
+ *
+ * @author luobosi@2dfire.com
+ * @since 2017-12-05
+ */
+@Data
+@AllArgsConstructor
+public class Person {
+    /** 用户名 */
+    private String username ;
+    /** 年龄 */
+    private Integer age;
+    /** 性别 */
+    private  String sex;
+
+    public Person() {
+        System.out.println("这个是无参数的构造器！");
+    }
+
+    public void show(){
+        System.out.println("无参数的show()方法");
+    }
+
+    public void show(String str){
+        System.out.println("带参数的show()方法" + str);
+    }
+
+    public void showInfo(String name, int age){
+        System.out.println("我的名字是："+name+"\t\t年龄："+age);
+
+    }
+    private  String eat(){
+        return "我很能吃！";
+    }
+
+}
