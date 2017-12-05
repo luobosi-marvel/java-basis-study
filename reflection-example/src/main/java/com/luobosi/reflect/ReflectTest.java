0fea8b9086aacbd6130a2e14451fd977d2183719
luobosi <luobosi@2dfire.com>
2017-12-05 13:21:29 +0800

    通过反射获取私有属性

--- reflection-example/src/main/java/com/luobosi/reflect/ReflectTest.java
+++ reflection-example/src/main/java/com/luobosi/reflect/ReflectTest.java
@@ -8,6 +8,7 @@ import com.luobosi.reflect.model.Person;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 
 /**
  * ReflectTest
@@ -52,19 +53,23 @@ public class ReflectTest {
         // 2. 得到对应的构造器
         // ① 获取 Person 对象的所对应的类的全部的 public 的构造器
         Constructor<?>[] constructors = classPerson.getConstructors();
+
         System.out.println(constructors);
         System.out.println();
-
         System.out.println("-------------------------->");
+
         for (Constructor constructor : constructors) {
             System.out.println("constructor:"+constructor);
         }
         System.out.println();
         System.out.println("-------------------------->");
+
         // ② 获取person对象所对应类的全部构造器
         Constructor[] persons = classPerson1.getDeclaredConstructors();
+
         System.out.println("person:"+persons);
         System.out.println();
+        System.out.println("-------------------------->");
 
         // 3.① 获取带参数的构造函数
         Constructor conPerson1 =  classPerson1.getConstructor(String.class, Integer.class, String.class);
@@ -74,11 +79,30 @@ public class ReflectTest {
         Person person1 = (Person) conPerson1.newInstance("国色天香", 20, "女");
         //   ② (2)创建无参数的实例
         Person person2 = (Person) conPerson2.newInstance(null);
-
-        System.out.println();
-        System.out.println("-------------------------->");
-        // 通过实例对象调用show()方法
+        // 通过实例对象调用 show()方法
         person1.show("国色天香");
         person2.show();
+    }
+
+    /**
+     * 通过反射获取私有属性
+     *
+     * @throws Exception 抛出异常
+     */
+    @Test
+    public void testGetPrivateField() throws Exception {
+        // 1. 获取 Class 对象
+        Class<?> classPerson = Class.forName("com.luobosi.reflect.model.Person");
+        // 2. 获取构造函数
+        Constructor<?> constructor = classPerson.getConstructor(null);
+        // 3. 创建一个实例
+        Person person = (Person) constructor.newInstance(null);
+        // 4. 根据字段名获取字段对象
+        Field username = classPerson.getDeclaredField("username");
+        // 设置私有属性可以访问
+        username.setAccessible(true);
+        username.set(person, "marvel");
+
+        System.out.println("名字：" + person.getUsername());
     }
 }
