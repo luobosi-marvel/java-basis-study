1a8433fb070cb2cad3db3c59699f283ab4b90380
luobosi <luobosi@2dfire.com>
2017-12-06 13:18:45 +0800

    自定义类加载器

---
+++ dynamic-proxy/src/main/java/com/luobosi/proxy/myproxy/MyClassLoader.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2009-2016 Hangzhou 2Dfire Technology Co., Ltd. All rights reserved
+ */
+package com.luobosi.proxy.myproxy;
+
+/**
+ * MyClassLoader
+ * 自定义类加载器
+ *
+ * @author luobosi@2dfire.com
+ * @since 2017-12-06
+ */
+public class MyClassLoader extends ClassLoader{
+
+}
