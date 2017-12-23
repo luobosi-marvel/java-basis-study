8d7c1dc1f477916ffa249486d9dc0bd83a8cb07d
luobosi <luobosi@2dfire.com>
2017-12-23 18:13:14 +0800

    JUC 练习

--- juc-example/src/main/java/com.luobosi.study.juc/block/queue/ArrayBlockingQueueDemo.java
+++ juc-example/src/main/java/com.luobosi.study.juc/block/queue/ArrayBlockingQueueDemo.java
@@ -3,6 +3,8 @@
  */
 package com.luobosi.study.juc.block.queue;
 
+import java.util.concurrent.ArrayBlockingQueue;
+
 /**
  * ArrayBlockingQueueDemo
  * 使用数组结构的阻塞队列
@@ -12,4 +14,22 @@ package com.luobosi.study.juc.block.queue;
  */
 public class ArrayBlockingQueueDemo {
 
+    private static ArrayBlockingQueue<Integer> queue = new ArrayBlockingQueue<>(20);
+
+    public static void main(String[] args) {
+        for (int i= 0; i < 20; i++) {
+            queue.add(i);
+        }
+
+        // 如果获取不到元素，直接返回 null
+        // 返回 null 有三个点：
+        // 1. println 遇到 null 元素会直接返回 null 字符串
+        // 1. String 遇到 null 元素会直接返回 null 字符串
+        // 1. Integer 遇到 null 元素会直接返回 null 字符串
+        for (int i = 0; i < 22; i ++) {
+            new Thread(() -> System.out.println(queue.poll())).start();
+        }
+
+
+    }
 }
