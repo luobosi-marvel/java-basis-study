31f98e8dd6098b6371fa7086875fa95f66c5870e
luobosi <luobosi@2dfire.com>
2018-08-10 15:26:44 +0800

    将字符串转换成数字

--- data-struct/src/main/java/com/luobosi/study/ds/A.java
+++ data-struct/src/main/java/com/luobosi/study/ds/A.java
@@ -3,6 +3,8 @@
  */
 package com.luobosi.study.ds;
 
+import java.util.Scanner;
+
 /**
  * A
  *
@@ -12,10 +14,61 @@ package com.luobosi.study.ds;
 public class A {
 
     public static void main(String[] args) {
-        {
-            byte[] placeholder = new byte[64 * 1024 * 1024];
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("输入一个串字符串：");
+        String str = scanner.nextLine();
+
+
+        int result = 0;
+        boolean negative = false;
+
+        int i = 0;
+        int len = str.length();
+        if (i < len) {
+            char flag = str.charAt(0);
+            // 说明是负数
+            if (flag == '-') {
+                negative = true;
+                i = 1;
+                len = str.length() - 1;
+            }
+
+            while (len > 0) {
+                len--;
+                int pow = (int) Math.pow(10, len);
+                char ch = str.charAt(i++);
+                int f;
+                if (ch == 49) {
+                    f = 1;
+                } else if (ch == 50) {
+                    f = 2;
+                } else if (ch == 51) {
+                    f = 3;
+                } else if (ch == 52) {
+                    f = 4;
+                } else if (ch == 53) {
+                    f = 5;
+                } else if (ch == 54) {
+                    f = 6;
+                } else if (ch == 55) {
+                    f = 7;
+                } else if (ch == 56) {
+                    f = 8;
+                } else if (ch == 57) {
+                    f = 9;
+                } else {
+                    f = 0;
+                }
+                result += f * pow;
+
+            }
         }
-        System.gc();
+
+        result = negative ? -result : result;
+        System.out.println(result);
+
     }
 
 }
