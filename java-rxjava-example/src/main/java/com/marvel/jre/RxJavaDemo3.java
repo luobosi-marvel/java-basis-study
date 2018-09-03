da2f1c1cd6582ce2700060bc6689a2214e647c3c
luobosi <luobosi@2dfire.com>
2018-09-04 02:20:57 +0800

    RxJava 创建操作符 学习

---
+++ java-rxjava-example/src/main/java/com/marvel/jre/RxJavaDemo3.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2009-2016 Hangzhou 2Dfire Technology Co., Ltd. All rights reserved
+ */
+package com.marvel.jre;
+
+/**
+ * RxJavaDemo3
+ * 创建操作符
+ * RxJava 的创建操作符主要包括如下内容：
+ * 1. just(): 将一个或多个对象转换成发射这个或这些对象的一个 Observable
+ * 2. from(): 将一个 Iterable、一个 Future 或者一个数组转换成一个 Observable
+ * 3. create(): 使用一个函数从头创建一个 Observable
+ * 4. defer(): 只有当订阅者才创建 Observable，为每个订阅者创建一个新的 Observable
+ * 5. range(): 创建一个发射指定范围的整数序列的 Observable
+ * 6. interval(): 创建一个按照给定的时间间隔发射整数序列的 Observable
+ * 7. timer(): 创建一个给定的延时之后发射单个数据的 Observable
+ * 8. empty(): 创建一个什么都不做直接通知完成的 Observable
+ * 9. error(): 创建一个什么都不做直接通知错误的 Observable
+ * 10. never(): 创建一个不发射任何数据的 Observable
+ *
+ * @author luobosi@2dfire.com
+ * @since 2018-09-04
+ */
+public class RxJavaDemo3 {
+
+}
